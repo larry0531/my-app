ad464e14c0cf2fd92784ae36144231c8d55af1b1
Larry Zhang <>
2020-02-17 08:23:25 +1100

    simple redux pratise

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddToDo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+const App = () => (
+    <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+    </div>
+)
+export default App
