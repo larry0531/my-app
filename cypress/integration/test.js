2afc53af12e3c09b3b435473d65ce5f3735f918c
Larry Zhang <larry.zhang.job@gmail.com>
2020-05-27 21:14:54 +1000

    command fix

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('clicking "type" navigates to a new url', () => {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:3000')
 
     cy.contains('type').click()
 
